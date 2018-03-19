d5cd201b860be774fb15a8430ddb313f210ea9d3
Kenji Mukai <kensuka21@gmail.com>
2018-03-19 16:23:44 -0400

    React skeleton

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+var webpack = require('webpack');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var BUILD_DIR = path.resolve(__dirname, 'dist');
+var APP_DIR = path.resolve(__dirname, 'src');
+
+var config = {
+    entry: APP_DIR + '/index.js',
+    output: {
+        path: BUILD_DIR,
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                loaders: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/,
+                use: 'url-loader'
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: "babel-loader"
+            }, 
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: "babel-loader"
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: 'index.html'
+        })
+    ]
+};
+
+module.exports = config;
