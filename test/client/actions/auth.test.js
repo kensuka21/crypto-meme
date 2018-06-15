b8bc24e507f6e76e1e2a665a28cecbce57d755c8
Kenji Mukai <kensuka21@gmail.com>
2018-06-14 21:24:10 -0400

    Testing load user

--- test/client/actions/auth.test.js
+++ test/client/actions/auth.test.js
@@ -1,5 +1,13 @@
+import * as actionTypes from '../../../src/client/actions/auth/actionTypes';
+import { loadAuthUser } from '../../../src/client/actions/auth';
+
 describe('Auth actions', () => {
   it('should create an action to load User', () => {
-    
+    const authUser = {};
+    const expectedAction = {
+      type: actionTypes.LOAD_AUTH_USER_SUCCESS,
+      authUser
+    };
+    expect(loadAuthUser(authUser)).toEqual(expectedAction);
   });
 });
