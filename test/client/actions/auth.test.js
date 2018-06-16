33ea89f7ad6378f01ff84403540b50dc29ebba70
Kenji Mukai <kensuka21@gmail.com>
2018-06-15 22:44:05 -0400

    Making test pass

--- test/client/actions/auth.test.js
+++ test/client/actions/auth.test.js
@@ -2,12 +2,15 @@ import * as actionTypes from '../../../src/client/actions/auth/actionTypes';
 import { loadAuthUser } from '../../../src/client/actions/auth';
 
 describe('Auth actions', () => {
-  it('should create an action to load User', () => {
+  it('should create an action to load User', async () => {
+    const dispatch = jest.fn();
     const authUser = {};
     const expectedAction = {
       type: actionTypes.LOAD_AUTH_USER_SUCCESS,
       authUser
     };
-    expect(loadAuthUser(authUser)).toEqual(expectedAction);
+
+    await loadAuthUser(authUser)(dispatch);
+    expect(dispatch).toBeCalledWith(expectedAction);
   });
 });
