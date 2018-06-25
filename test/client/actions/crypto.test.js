12026134d70c02d9f7215bc69fad45c5aa36e9a3
Kenji Mukai <kensuka21@gmail.com>
2018-06-25 11:31:30 -0400

    Crypto action test.

---
+++ test/client/actions/crypto.test.js
@@ -0,0 +1,8 @@
+import * as actionTypes from '../../../src/client/actions/crypto/actionTypes';
+import { loadAuthUser } from '../../../src/client/actions/crypto';
+
+describe('Crypto actions', () => {
+  it('should create an action to load crypto price', async () => {
+    
+  });
+});
